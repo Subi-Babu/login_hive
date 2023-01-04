c0e282e2ff0d28f23829e492859320fde4be0462
Subi-Babu <subiaby4613@gmail.com>
2023-01-04 11:18:05 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:login_hive/models/user_model.dart';
+import 'screen/login.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+   Hive.registerAdapter(UserModelAdapter());
+  await Hive.openBox<UserModel>('user');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: LoginScreen(),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.grey[200]),
+    );
+  }
+  }
